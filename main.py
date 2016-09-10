3c456e457b982974832f9dbca742dc406f58d10b
Hunter Heidenreich <hunter.scott.heidenreich@gmail.com>
2016-09-10 11:59:39 -0400

    Alter naming convention for functions

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ FIREBASE_CONFIG = pickle.load(open('fb_conf.p', 'rb'))
 FIREBASE_LOGIN = pickle.load(open('fb_login.p', 'rb'))
 
 
-def checkOrders():
+def check_orders():
     global STAT
     data = requests.get(STAT['ORDER_URL'], auth=HTTPBasicAuth(PRINTFUL_CONFIG['user'], PRINTFUL_CONFIG['passw']))
     orderCount = data.json()['paging']['total']
@@ -29,55 +29,61 @@ def checkOrders():
         unprocessed = orderCount - STAT['ORDER_COUNT']
         for index in range(0, unprocessed):
             if formatted[index]['external_id']:
-                processItems(formatted[index]['items'])
+                proccess_items(formatted[index]['items'])
             STAT['ORDER_COUNT'] += 1
         if unprocessed > 0:
-            updateFirebase()
+            update_firebase()
 
     print("EXP", STAT['EXP'])
 
-def processItems(items):
+
+def proccess_items(items):
     global STAT
     for item in items:
         print(item['name'])
         if 'Shirt' in item['name']:
             print("This is a shirt")
-            addExp(STAT['SHIRT'])
+            add_exp(STAT['SHIRT'])
         elif 'Tank' in item['name']:
             print("This is a tank")
-            addExp(STAT['TANK'])
+            add_exp(STAT['TANK'])
         elif 'Sock' in item['name']:
             print("These are socks")
-            addExp(STAT['SOCK'])
+            add_exp(STAT['SOCK'])
         elif 'Leggings' in item['name']:
             print("These are leggings")
-            addExp(STAT['LEGGING'])
+            add_exp(STAT['LEGGING'])
 
-def addExp(addition):
+
+def add_exp(addition):
     global STAT
     STAT['EXP'] += addition
 
-def initFirebase():
+
+def init_firebase():
     global STAT, USER
     USER = FIREBASE.auth().refresh(USER['refreshToken'])
     db = FIREBASE.database()
     STAT['EXP'] = db.child("EXP").get(USER['idToken']).val()
 
-def updateFirebase():
+
+def update_firebase():
     print("Updated orders.")
     global STAT, USER
     FIREBASE.auth().refresh(USER['refreshToken'])
     db = FIREBASE.database()
     db.update({'EXP': STAT['EXP']}, USER['idToken'])
-    writeData()
+    write_data()
 
-def loadData():
+
+def load_data():
     global STAT
     data = open('data.txt', 'r')
     STAT['ORDER_COUNT'] = int(data.read())
     data.close()
 
-def writeData():
+
+def write_data():
     text_file = open("data.txt", "w")
     text_file.write("%s" % STAT['ORDER_COUNT'])
     text_file.close()
@@ -86,7 +92,7 @@ def writeData():
 FIREBASE = pyrebase.initialize_app(FIREBASE_CONFIG)
 USER = FIREBASE.auth().sign_in_with_email_and_password(FIREBASE_LOGIN['email'], FIREBASE_LOGIN['password'])
 while True:
-    loadData()
-    initFirebase()
-    checkOrders()
+    load_data()
+    init_firebase()
+    check_orders()
     sleep(900)
